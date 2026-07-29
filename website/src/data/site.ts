10ba616b56f773d04204256b5bdc99059a132eb9
Ouvill <contact@ouvill.net>
2026-07-30 01:34:22 +0900

    Bump font version to 1.028

--- website/src/data/site.ts
+++ website/src/data/site.ts
@@ -10,7 +10,7 @@ export const SITE = {
     socialHandle: "@ouvill",
     socialUrl: "https://twitter.com/ouvill",
   },
-  version: "1.027",
+  version: "1.028",
   releaseUrl: "https://github.com/ouvill/nobigoe-font/releases",
   repositoryUrl: "https://github.com/ouvill/nobigoe-font",
   supportUrl: "https://github.com/sponsors/ouvill",
